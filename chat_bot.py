1871963135a455c9c44196dbfed1814e1183b299
Anastasia Maryasova <39577497+amaryasova@users.noreply.github.com>
2023-03-20 18:05:41 +0700

    Update chat_bot.py

--- chat_bot.py
+++ chat_bot.py
@@ -1,5 +1,6 @@
+from datetime import datetime
 import psycopg2
-conn = psycopg2.connect(dbname='komandor', user='postgres', password='xxx', host='194.67.65.146')
+conn = psycopg2.connect(dbname='komandor', user='postgres', password='', host='194.67.65.146')
 cursor = conn.cursor()
 import telebot
 from telebot import types
@@ -10,19 +11,19 @@ bot = telebot.TeleBot('6069432036:AAG4sD4wgScTQXZ9quIk_Lq_44AKCEDOQe8')
 def start(message):
 
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton("Внести данные о продаже")
+    btn1 = types.KeyboardButton('Внести данные о продаже')
     markup.add(btn1)
-    bot.send_message(message.from_user.id, "Данные внесены", reply_markup=markup)
 
-@bot.message_handler(content_types=['bigint'])
+@bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-
-    if message.text.type is bigint:
-        cursor.execute("INSERT INTO bot (date, value) VALUES (date.today(), message.text)")
-		conn.commit()  
-		print("Данные добавлены")
- 
-cursor.close()
-conn.close()
+    if message.text == 'Внести данные о продаже':
+        bot.send_message(message.from_user.id, 'Введите сумму продажи')
+    if message.text.isdigit() is True:
+        dv = (datetime.now(), int(message.text))
+        print(dv)
+        cursor.execute("INSERT INTO bot (date, value) VALUES (%s, %s)", dv)
+        conn.commit()
+        print('Данные добавлены')
+        bot.send_message(message.from_user.id, 'Данные добавлены')
 
 bot.polling(none_stop=True, interval=0) #обязательная для работы бота часть
